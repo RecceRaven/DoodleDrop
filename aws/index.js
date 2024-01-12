174d1081b474c2e3e67cf7b38f9b5bb496affea4
Kay <hiebertkay@gmail.com>
2024-01-12 09:47:38 -0500

    cleaned up code

---
+++ aws/index.js
@@ -0,0 +1,8 @@
+const imageForm = document.querySelector('#imageForm')
+const imageInput = document.querySelector("imageInput")
+
+imageForm.addEventListener("submit"), async event => {
+  event.preventDefault()
+  const file = imageInput.files(0)
+}
+
